6e092c3290e473a3a2a2d32febe86d2bac76a709
Tony Lea <tony@tonys-mbp-2.mynetworksettings.com>
2024-12-12 07:16:08 -0500

    Upgrading to Inertia 2 and Tailwind v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
@@ -7,5 +9,7 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        tailwindcss(),
+        vue(),
     ],
 });
