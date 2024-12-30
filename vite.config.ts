55f55fb4e66bdd6b3d34d09fd7b02a61f0de07d0
Tony Lea <tony@devdojo.com>
2024-12-30 16:36:17 -0500

    downgrading back to Tailwind v3 until official release is out

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,36 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', '@/app.js'],
+            input: ['resources/js/app.ts'],
             refresh: true,
         }),
-        tailwindcss(),
-        vue(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './resources/js'),
+        },
+    },
+    css: {
+        postcss: {
+            plugins: [
+                tailwindcss,
+                autoprefixer,
+            ],
+        },
+    },
 });
