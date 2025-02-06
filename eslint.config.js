faed6823520522d4163fdfcdb23ba98d35548101
Tony Lea <tony@devdojo.com>
2025-02-06 16:01:42 -0500

    fixing the formatting in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,19 @@
 import prettier from 'eslint-config-prettier';
 import vue from 'eslint-plugin-vue';
 
-import {
-    defineConfigWithVueTs,
-    vueTsConfigs,
-} from '@vue/eslint-config-typescript'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
 
 export default defineConfigWithVueTs(
     vue.configs['flat/essential'],
-    vueTsConfigs.recommended, {
+    vueTsConfigs.recommended,
+    {
         ignores: ['vendor', 'node_modules', 'public', 'bootstrap/ssr', 'tailwind.config.js', 'resources/js/components/ui/*'],
-    }, {
+    },
+    {
         rules: {
             'vue/multi-word-component-names': 'off',
-            '@typescript-eslint/no-explicit-any': 'off'
-        }
+            '@typescript-eslint/no-explicit-any': 'off',
+        },
     },
-    prettier
-)
+    prettier,
+);
