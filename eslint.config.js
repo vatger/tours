f894fb0c76dfae16d582082319b3f36760ce6dbb
Tony Lea <tony@devdojo.com>
2025-02-06 15:24:41 -0500

    removing husky from the Vue starter kit and implementing the new eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,68 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+import typescript from 'typescript-eslint';
+import vue from 'eslint-plugin-vue';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    js.configs.recommended,
+    ...typescript.configs.recommended,
+    {
+        ...vue.configs.flat.recommended,
+        ...vue.configs["flat/strongly-recommended"],
+        ...vue.configs["flat/essential"],
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
+        },
+        rules: {
+            "vue/match-component-import-name": "warn",
+            "vue/match-component-file-name": [
+                "error",
+                {
+                    extensions: ["vue"],
+                    shouldMatchCase: true,
+                },
+            ],
+            "vue/component-definition-name-casing": ["error", "PascalCase"],
+            "vue/block-tag-newline": [
+                "warn",
+                {
+                    singleline: "always",
+                    multiline: "always",
+                    maxEmptyLines: 0,
+                },
+            ],
+            "vue/html-self-closing": [
+                "error",
+                {
+                    html: {
+                        void: "always",
+                        normal: "never",
+                        component: "always",
+                    },
+                    svg: "always",
+                    math: "always",
+                },
+            ],
+            "vue/require-default-prop": "off",
+        },
+    },
+    {
+        plugins: {
+            "@typescript-eslint": tseslint.plugin,
+        },
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                project: true,
+            },
+        },
+    },
+    {
+        ignores: ['vendor', 'node_modules', 'public', 'bootstrap/ssr', 'tailwind.config.js'],
+    },
+    prettier, // Turn off all rules that might conflict with Prettier
+];
