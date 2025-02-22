c36df08cc7954825b905411ef46cd3184b4a834b
Tony Lea <tony@devdojo.com>
2025-02-22 10:35:31 -0500

    cleanup

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -7,13 +7,6 @@ defineProps<{
     laravelVersion: string;
     phpVersion: string;
 }>();
-
-function handleImageError() {
-    document.getElementById('screenshot-container')?.classList.add('!hidden');
-    document.getElementById('docs-card')?.classList.add('!row-span-1');
-    document.getElementById('docs-card-content')?.classList.add('!flex-row');
-    document.getElementById('background')?.classList.add('!hidden');
-}
 </script>
 
 <template>
