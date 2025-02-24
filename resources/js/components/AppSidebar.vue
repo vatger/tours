93d3392203a40b04e006d794d92b7c4e67376987
Taylor Otwell <taylor@laravel.com>
2025-02-24 10:09:27 -0600

    formatting

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -4,9 +4,9 @@ import NavMain from '@/components/NavMain.vue';
 import NavUser from '@/components/NavUser.vue';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
+import { Link } from '@inertiajs/vue3';
 import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
-import { Link } from '@inertiajs/vue3';
 
 const mainNavItems: NavItem[] = [
     {
