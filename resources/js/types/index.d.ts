767ac75bc773dee3f5771d6a89b152ab2085a849
Steve McDougall <JustSteveKing@users.noreply.github.com>
2025-04-08 14:27:15 +0100

    Add PaginatedResult Interface
    Adding a `PaginatedResult` interface with generics to allow for better typed pagination between Laravel and Inertia/Vue

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -36,4 +36,28 @@ export interface User {
     updated_at: string;
 }
 
+export interface PaginatedResult<T> {
+  data: T[];
+  links: {
+    first: string;
+    last: string;
+    next?: string;
+    prev?: string;
+  };
+  meta: {
+    current_page: number;
+    from?: number;
+    last_page: number;
+    links: {
+      active: boolean;
+      label: string;
+      url?: string;
+    }[];
+    path: string;
+    per_page: number;
+    to?: number;
+    total: number;
+  };
+};
+
 export type BreadcrumbItemType = BreadcrumbItem;
