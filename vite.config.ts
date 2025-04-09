3a04d610cf281e62ade9e6a23f31d162bc45cf2f
Vanlalruata Hnamte <vanlalruata.hnamte@gmail.com>
2025-04-09 18:42:15 +0530

    Upgrade to TailwindCSS 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import vue from '@vitejs/plugin-vue';
-import autoprefixer from 'autoprefixer';
 import laravel from 'laravel-vite-plugin';
 import path from 'path';
 //import tailwindcss from 'tailwindcss';
@@ -29,10 +28,5 @@ export default defineConfig({
             '@': path.resolve(__dirname, './resources/js'),
             'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
         },
-    },
-    css: {
-        postcss: {
-            plugins: [tailwindcss],
-        },
-    },
+    },    
 });
