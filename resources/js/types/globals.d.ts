b7e2ce8c26d14ff74d51d1599cf1191a3621da75
Tim Kunze <tim.kunze4@gmail.com>
2025-04-17 13:09:15 +0200

    Move type declaration into definition file

--- resources/js/types/globals.d.ts
+++ resources/js/types/globals.d.ts
@@ -24,3 +24,16 @@ declare module '@vue/runtime-core' {
         $headManager: ReturnType<typeof createHeadManager>;
     }
 }
+
+// Extend ImportMeta interface for Vite...
+declare module 'vite/client' {
+    interface ImportMetaEnv {
+        readonly VITE_APP_NAME: string;
+        [key: string]: string | boolean | undefined;
+    }
+
+    interface ImportMeta {
+        readonly env: ImportMetaEnv;
+        readonly glob: <T>(pattern: string) => Record<string, () => Promise<T>>;
+    }
+}
