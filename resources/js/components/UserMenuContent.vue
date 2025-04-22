28925e1db3f1804aad103f9f2671360d447c33ca
Tony Lea <tony@devdojo.com>
2025-04-22 11:11:35 -0400

    Adding prefetch to settings and adding logout flush (#116)
    * Adding prefetch to settings and adding logout flush
    
    * Update UserMenuContent.vue
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- resources/js/components/UserMenuContent.vue
+++ resources/js/components/UserMenuContent.vue
@@ -2,12 +2,16 @@
 import UserInfo from '@/components/UserInfo.vue';
 import { DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator } from '@/components/ui/dropdown-menu';
 import type { User } from '@/types';
-import { Link } from '@inertiajs/vue3';
+import { Link, router } from '@inertiajs/vue3';
 import { LogOut, Settings } from 'lucide-vue-next';
 
 interface Props {
     user: User;
 }
+
+const handleLogout = () => {
+    router.flushAll();
+};
 
 defineProps<Props>();
 </script>
@@ -21,7 +25,7 @@ defineProps<Props>();
     <DropdownMenuSeparator />
     <DropdownMenuGroup>
         <DropdownMenuItem :as-child="true">
-            <Link class="block w-full" :href="route('profile.edit')" as="button">
+            <Link class="block w-full" :href="route('profile.edit')" prefetch as="button">
                 <Settings class="mr-2 h-4 w-4" />
                 Settings
             </Link>
@@ -29,7 +33,7 @@ defineProps<Props>();
     </DropdownMenuGroup>
     <DropdownMenuSeparator />
     <DropdownMenuItem :as-child="true">
-        <Link class="block w-full" method="post" :href="route('logout')" as="button">
+        <Link class="block w-full" method="post" :href="route('logout')" @click="handleLogout" as="button">
             <LogOut class="mr-2 h-4 w-4" />
             Log out
         </Link>
