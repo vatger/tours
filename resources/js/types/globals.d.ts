37dc294f78b7f008f98872a92328fd27565ef2f8
drsoft28 <dif.rabie@gmail.com>
2025-04-23 10:38:15 +0100

    Update globals.d.ts: add route to module vue for ignore apeearing error in eslint

--- resources/js/types/globals.d.ts
+++ resources/js/types/globals.d.ts
@@ -3,3 +3,8 @@ import type { route as routeFn } from 'ziggy-js';
 declare global {
     const route: typeof routeFn;
 }
+declare module 'vue' {
+    interface ComponentCustomProperties {
+         route: typeof routeFn
+    }
+}
