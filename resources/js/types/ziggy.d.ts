0319f1e42f8a361da6e57ab193cbdcbd9f1fa855
Tim Kunze <tim.kunze4@gmail.com>
2025-04-24 14:46:09 +0200

    Simplify route type

--- resources/js/types/ziggy.d.ts
+++ resources/js/types/ziggy.d.ts
@@ -1,8 +1,7 @@
-import { RouteParams, Router } from 'ziggy-js';
+import { route } from 'ziggy-js';
 
 declare global {
-    function route(): Router;
-    function route(name: string, params?: RouteParams<typeof name> | undefined, absolute?: boolean): string;
+    let route: typeof route;
 }
 
 declare module '@vue/runtime-core' {
