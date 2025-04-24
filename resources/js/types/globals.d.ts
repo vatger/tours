ecd5488d304cdccb92e57a2a3efdacb1afa8692a
Tim Kunze <tim.kunze4@gmail.com>
2025-04-24 14:45:06 +0200

    Refactor page props: Allows usage of usePage without having to explicitly type the props

--- resources/js/types/globals.d.ts
+++ resources/js/types/globals.d.ts
@@ -16,24 +16,3 @@ declare module 'vite/client' {
 declare module '@inertiajs/core' {
     interface PageProps extends InertiaPageProps, AppPageProps {}
 }
-
-declare module '@vue/runtime-core' {
-    interface ComponentCustomProperties {
-        $inertia: typeof Router;
-        $page: Page;
-        $headManager: ReturnType<typeof createHeadManager>;
-    }
-}
-
-// Extend ImportMeta interface for Vite...
-declare module 'vite/client' {
-    interface ImportMetaEnv {
-        readonly VITE_APP_NAME: string;
-        [key: string]: string | boolean | undefined;
-    }
-
-    interface ImportMeta {
-        readonly env: ImportMetaEnv;
-        readonly glob: <T>(pattern: string) => Record<string, () => Promise<T>>;
-    }
-}
