e5b47d4d7f330de0f6be5550efd5f5cf861aae57
Tim Kunze <tim.kunze4@gmail.com>
2025-04-24 14:52:21 +0200

    Simplify ssr logic: Removed unnecessary config and route declaration

--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -2,7 +2,7 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import createServer from '@inertiajs/vue3/server';
 import { renderToString } from '@vue/server-renderer';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createSSRApp, DefineComponent, h } from 'vue';
+import { createApp, DefineComponent, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
@@ -14,14 +14,13 @@ createServer((page) =>
         title: (title) => `${title} - ${appName}`,
         resolve: resolvePage,
         setup: ({ App, props, plugin }) =>
-            createSSRApp({ render: () => h(App, props) })
+            createApp({ render: () => h(App, props) })
                 .use(plugin)
                 .use(ZiggyVue, {
                     ...page.props.ziggy,
                     location: new URL(page.props.ziggy.location),
                 }),
     }),
-    { cluster: true },
 );
 
 function resolvePage(name: string) {
