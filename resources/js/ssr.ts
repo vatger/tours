fe09476a9ac0cb1ce5a048ba1c900a019259c649
Tim Kunze <tim.kunze4@gmail.com>
2025-04-24 14:52:21 +0200

    Simplify ssr logic: Removed unnecessary config and route declaration

--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -2,8 +2,8 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import createServer from '@inertiajs/vue3/server';
 import { renderToString } from '@vue/server-renderer';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createSSRApp, DefineComponent, h } from 'vue';
-import { route as ziggyRoute } from 'ziggy-js';
+import { createApp, DefineComponent, h } from 'vue';
+import { ZiggyVue } from 'ziggy-js';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -13,30 +13,13 @@ createServer((page) =>
         render: renderToString,
         title: (title) => `${title} - ${appName}`,
         resolve: resolvePage,
-        setup({ App, props, plugin }) {
-            const app = createSSRApp({ render: () => h(App, props) });
-
-            // Configure Ziggy for SSR...
-            const ziggyConfig = {
-                ...page.props.ziggy,
-                location: new URL(page.props.ziggy.location),
-            };
-
-            // Create route function...
-            const route = (name: string, params?: any, absolute?: boolean) => ziggyRoute(name, params, absolute, ziggyConfig);
-
-            // Make route function available globally...
-            app.config.globalProperties.route = route;
-
-            // Make route function available globally for SSR...
-            if (typeof window === 'undefined') {
-                global.route = route;
-            }
-
-            app.use(plugin);
-
-            return app;
-        },
+        setup: ({ App, props, plugin }) =>
+            createApp({ render: () => h(App, props) })
+                .use(plugin)
+                .use(ZiggyVue, {
+                    ...page.props.ziggy,
+                    location: new URL(page.props.ziggy.location),
+                }),
     }),
 );
 
