2986a6f634f1ea5117cba85b750201d348bf47a8
Tim Kunze <tim.kunze4@gmail.com>
2025-04-24 15:13:44 +0200

    Fix createSSRApp call and set clustering to true

--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -2,7 +2,7 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import createServer from '@inertiajs/vue3/server';
 import { renderToString } from '@vue/server-renderer';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createApp, DefineComponent, h } from 'vue';
+import { createSSRApp, DefineComponent, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
@@ -14,13 +14,14 @@ createServer((page) =>
         title: (title) => `${title} - ${appName}`,
         resolve: resolvePage,
         setup: ({ App, props, plugin }) =>
-            createApp({ render: () => h(App, props) })
+            createSSRApp({ render: () => h(App, props) })
                 .use(plugin)
                 .use(ZiggyVue, {
                     ...page.props.ziggy,
                     location: new URL(page.props.ziggy.location),
                 }),
     }),
+    { cluster: true },
 );
 
 function resolvePage(name: string) {
