0e27117ffeb25e9693f4ce57a353fea8ae65798f
Peter Meijer <petermeijer83@gmail.com>
2025-04-28 00:39:03 +0200

    chore: set up AppServiceProvider; - Disable destructive commands on production - Enforce https on production - Enforce immutable dates - Enforce strict models - Unguard models - Use aggressive prefetching for Vite

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,11 +4,55 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void {}
 
-    public function boot(): void {}
+    public function boot(): void
+    {
+        $this->configureCommands();
+        $this->configureDates();
+        $this->configureModels();
+        $this->configureUrl();
+        $this->configureVite();
+
+        JsonResource::withoutWrapping();
+    }
+
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands($this->app->isProduction());
+    }
+
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict();
+
+        Model::unguard();
+    }
+
+    private function configureUrl(): void
+    {
+        URL::forceHttps(App::isProduction());
+    }
+
+    private function configureVite(): void
+    {
+        Vite::useAggressivePrefetching();
+    }
 }
