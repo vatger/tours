4082da666c491da8e464e32747a8ada76baea712
Peter Meijer <petermeijer83@gmail.com>
2025-04-28 00:55:29 +0200

    chore: enforce strict types in ArchTest.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,7 +7,6 @@ namespace Database\Factories;
 use App\Models\User;
 use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
--- tests/Unit/ArchTest.php
+++ tests/Unit/ArchTest.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+arch()->expect('App')->toUseStrictTypes();
 arch()->preset()->php();
 arch()->preset()->security();
 arch()->preset()->laravel();
