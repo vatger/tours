fb5e41d2a054fee4af0b38af698ab1dacf31075f
Peter Meijer <petermeijer83@gmail.com>
2025-04-28 00:49:27 +0200

    chore: fix phpstan issue

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Database\Factories;
 
 use App\Models\User;
+use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Hash;
@@ -21,7 +22,7 @@ class UserFactory extends Factory
      * @return array{
      *     name: string,
      *     email: string,
-     *     email_verified_at: Carbon,
+     *     email_verified_at: CarbonImmutable,
      *     password: string,
      *     remember_token: string
      * }
