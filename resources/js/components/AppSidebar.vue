5015fb6a8fa52dd478624ffca3d6f64c23fdde79
Peter Meijer <petermeijer83@gmail.com>
2025-04-28 16:06:09 +0200

    chore: update Github Repo link

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -20,7 +20,7 @@ const mainNavItems: NavItem[] = [
 const footerNavItems: NavItem[] = [
     {
         title: 'Github Repo',
-        href: 'https://github.com/laravel/vue-starter-kit',
+        href: 'https://github.com/peter-meijer/vue-starter-kit',
         icon: Folder,
     },
     {
