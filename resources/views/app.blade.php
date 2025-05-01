126bb03fb7e30b3f4ce782e9a953989a75968a9f
Tony Lea <tony@devdojo.com>
2025-05-01 15:01:51 -0400

    Adding vue page component to vite helper

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -40,7 +40,7 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         @routes
-        @vite(['resources/js/app.ts'])
+        @vite(["resources/js/app.ts", "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
