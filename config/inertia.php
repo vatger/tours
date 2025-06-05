80cfefd9b613fbaa04e613577be94868c8a04fd9
Taylor Otwell <taylor@laravel.com>
2025-06-05 14:44:17 -0500

    formatting

--- config/inertia.php
+++ config/inertia.php
@@ -7,26 +7,18 @@ return [
     | Server Side Rendering
     |--------------------------------------------------------------------------
     |
-    | These options configures if and how Inertia uses Server Side Rendering
-    | to pre-render the initial visits made to your application's pages.
+    | These options configure if and how Inertia uses Server Side Rendering
+    | to pre-render every initial visit made to your application's pages
+    | automatically. A separate rendering service should be available.
     |
-    | You can specify a custom SSR bundle path, or omit it to let Inertia
-    | try and automatically detect it for you.
-    |
-    | Do note that enabling these options will NOT automatically make SSR work,
-    | as a separate rendering service needs to be available. To learn more,
-    | please visit https://inertiajs.com/server-side-rendering
+    | See: https://inertiajs.com/server-side-rendering
     |
     */
 
     'ssr' => [
-
         'enabled' => true,
-
         'url' => 'http://127.0.0.1:13714',
-
         // 'bundle' => base_path('bootstrap/ssr/ssr.mjs'),
-
     ],
 
     /*
@@ -36,38 +28,30 @@ return [
     |
     | The values described here are used to locate Inertia components on the
     | filesystem. For instance, when using `assertInertia`, the assertion
-    | attempts to locate the component as a file relative to any of the
-    | paths AND with any of the extensions specified here.
+    | attempts to locate the component as a file relative to the paths.
     |
     */
 
     'testing' => [
-
         'ensure_pages_exist' => true,
 
         'page_paths' => [
-
             resource_path('js/pages'),
-
         ],
 
         'page_extensions' => [
-
             'js',
             'jsx',
             'svelte',
             'ts',
             'tsx',
             'vue',
-
         ],
 
     ],
 
     'history' => [
-
         'encrypt' => false,
-
     ],
 
 ];
