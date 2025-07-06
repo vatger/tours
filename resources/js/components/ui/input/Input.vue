d8514c97f8ee99c6fe1bd7b9062dcab74f4e7166
Rezunov Eduard <job-asd1239911@yandex.ru>
2025-07-06 18:32:27 +0400

    fix bug input focus

--- resources/js/components/ui/input/Input.vue
+++ resources/js/components/ui/input/Input.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from 'vue'
+import { HTMLAttributes, ref } from 'vue';
 import { cn } from '@/lib/utils'
 import { useVModel } from '@vueuse/core'
 
@@ -17,10 +17,18 @@ const modelValue = useVModel(props, 'modelValue', emits, {
   passive: true,
   defaultValue: props.defaultValue,
 })
+
+const inputRef = ref<HTMLInputElement | null>(null);
+
+defineExpose({
+    input: inputRef,
+    focus: () => inputRef.value?.focus()
+});
 </script>
 
 <template>
   <input
+    ref="inputRef"
     v-model="modelValue"
     data-slot="input"
     :class="cn(
