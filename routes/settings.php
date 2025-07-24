c5a26f56de788c4370e88cdc4bbfa1f9f968e818
Taylor Otwell <taylor@laravel.com>
2025-07-24 12:54:12 -0500

    Update settings.php

--- routes/settings.php
+++ routes/settings.php
@@ -13,6 +13,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('settings/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('settings/password', [PasswordController::class, 'edit'])->name('password.edit');
+
     Route::put('settings/password', [PasswordController::class, 'update'])
         ->middleware('throttle:6,1')
         ->name('password.update');
