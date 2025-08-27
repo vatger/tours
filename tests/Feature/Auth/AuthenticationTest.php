4ac620f979ec858b4da127bcdd784732089fbd72
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2025-08-27 11:02:31 -0400

    Update AuthenticationTest.php

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -21,7 +21,7 @@ class AuthenticationTest extends TestCase
     {
         $user = User::factory()->create();
 
-        $response = $this->post(route('login'), [
+        $response = $this->post(route('login.store'), [
             'email' => $user->email,
             'password' => 'password',
         ]);
