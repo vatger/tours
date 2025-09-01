a020197e4ca2950d5a156e4c0ae43e4eadb35f56
Pushpak Chhajed <pushpak1300@gmail.com>
2025-09-01 12:51:13 +0530

    Refactor TwoFactorAuthenticationRequest namespace

--- app/Http/Controllers/Settings/TwoFactorAuthenticationController.php
+++ app/Http/Controllers/Settings/TwoFactorAuthenticationController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Settings;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\TwoFactorAuthenticationRequest;
+use App\Http\Requests\Settings\TwoFactorAuthenticationRequest;
 use Illuminate\Routing\Controllers\HasMiddleware;
 use Illuminate\Routing\Controllers\Middleware;
 use Inertia\Inertia;
--- app/Http/Requests/Settings/TwoFactorAuthenticationRequest.php
+++ app/Http/Requests/Settings/TwoFactorAuthenticationRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\Auth;
+namespace App\Http\Requests\Settings;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Laravel\Fortify\Features;
