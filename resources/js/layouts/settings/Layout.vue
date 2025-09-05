dbef74dff38d21faa3bd545e36667c03d271fb9a
Pushpak Chhajed <pushpak1300@gmail.com>
2025-09-05 20:03:13 +0530

    Standardise routes name

--- resources/js/layouts/settings/Layout.vue
+++ resources/js/layouts/settings/Layout.vue
@@ -3,16 +3,16 @@ import Heading from '@/components/Heading.vue';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
 import { toUrl, urlIsActive } from '@/lib/utils';
-import { appearance } from '@/routes';
+import { edit as editAppearance } from '@/routes/appearance';
 import { edit as editPassword } from '@/routes/password';
-import { edit } from '@/routes/profile';
+import { edit as editProfile } from '@/routes/profile';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
 
 const sidebarNavItems: NavItem[] = [
     {
         title: 'Profile',
-        href: edit(),
+        href: editProfile(),
     },
     {
         title: 'Password',
@@ -20,7 +20,7 @@ const sidebarNavItems: NavItem[] = [
     },
     {
         title: 'Appearance',
-        href: appearance(),
+        href: editAppearance(),
     },
 ];
 
