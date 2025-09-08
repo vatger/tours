d12cb117fecc6f0753e2a8598f9843cfc8ff0ee2
ChanDarong <chandarong333@gmail.com>
2025-09-08 11:41:54 +0700

    Refactor urlIsActive function to improve URL matching logic

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -7,7 +7,17 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function urlIsActive(urlToCheck: NonNullable<InertiaLinkProps['href']>, currentUrl: string) {
-    return toUrl(urlToCheck) === currentUrl;
+    const checkUrl = toUrl(urlToCheck);
+
+    if (checkUrl === '/' && currentUrl === '/') {
+        return true;
+    }
+
+    if (checkUrl !== '/' && currentUrl.startsWith(checkUrl)) {
+        return true;
+    }
+
+    return false;
 }
 
 export function toUrl(href: NonNullable<InertiaLinkProps['href']>) {
