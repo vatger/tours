f00cd8df9b874ae4934dc5506257b039f48bf6f0
James Brooks <james@alt-three.com>
2025-10-07 16:13:42 +0100

    Add scheduler

--- config/app.php
+++ config/app.php
@@ -15,6 +15,8 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+    'heartbeat_url' => env('HEARTBEAT_URL'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
