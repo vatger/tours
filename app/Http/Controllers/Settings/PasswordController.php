eb5e27d864f65747ee67984ee02342e3a6c7f830
Punyapal Shah <53343069+MrPunyapal@users.noreply.github.com>
2025-10-12 22:50:13 +0530

    lint

--- app/Http/Controllers/Settings/PasswordController.php
+++ app/Http/Controllers/Settings/PasswordController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Settings;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
 use Inertia\Inertia;
 use Inertia\Response;
