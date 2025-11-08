77374ff602c2bdf8595f4d0bf294fa5d7a667490
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2025-11-08 21:33:50 +0100

    wip

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\TourLegUser;
 use App\Models\User;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
